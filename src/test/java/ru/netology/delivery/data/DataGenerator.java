70f85bc38f7567e329b3c0749adcc0d8db8a1faf
Emma Paris <misyura.c@yandex.ru>
2024-11-05 03:42:18 +0300

    commit

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,57 @@
+package ru.netology.delivery.data;
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        var cities = new String[]{"Волгоград", "Воронеж", "Астрахань", "Барнаул", "Тамбов","Красноярск", "Калининград", "Краснодар", "Самара", "Саратов", "Владимир", "Новосибирск", "Рязань", "Ижевск", "Москва",
+                "Санкт-Петербург", "Ростов-на-Дону"};
+        return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String generatePhone(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+    public static String generateWrongPhone(String locale){
+        var faker = new Faker(new Locale(locale));
+        return faker.numerify("####");
+
+    }
+
+    public static class Registration {
+        private Registration() {
+
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+
+}
